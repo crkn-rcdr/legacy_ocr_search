997d0e0543e41de5a89be100ad762bf269d64b87
Brittny Lapierre <brittny.lapierre@gmail.com>
2023-09-21 19:44:57 -0400

    Search using param

--- app/controllers/search_object_controller.rb
+++ app/controllers/search_object_controller.rb
@@ -1,7 +1,7 @@
 class SearchObjectController < ApplicationController
     def search
         rsolr = RSolr.connect :url => 'http://tokaji.tor.c7a.ca:8983/solr/cosearch2'
-        @request = rsolr.get 'select', :params => {:q => '*:*'}
+        @request = rsolr.get 'select', :params => {:q => 'tx:' + params[:q]} 
         puts "test"
         puts @request['response']['docs']
         @results = @request['response']['docs']
