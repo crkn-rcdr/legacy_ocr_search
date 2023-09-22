dec7fd763b94d56763b64fa73ad37842e0d014f2
Brittny Lapierre <brittny.lapierre@gmail.com>
2023-09-22 14:57:25 -0400

    Handle external prefix set

--- app/components/legacy_ocr_search_component.rb
+++ app/components/legacy_ocr_search_component.rb
@@ -1,14 +1,17 @@
 require 'view_component/version'
 class LegacyOcrSearchComponent < ViewComponent::Base
-    def initialize(documentId:, term:)
+    def initialize(documentId:, term:, prefix:)
         @documentId = documentId
         @term = term
+        @prefix = prefix
         rsolr = RSolr.connect :url => 'http://tokaji.tor.c7a.ca:8983/solr/cosearch2'
         @legacy_ocr_search_request= rsolr.get 'select', :params => {
-            :q => 'tx:' + term + ' AND ' + 'pkey:' + 'oocihm.' + @documentId
+            :q => 'tx:' + term + ' AND ' + 'pkey:' + @prefix + '.' + @documentId
         } 
-        puts "test"
-        puts @legacy_ocr_search_request['response']['docs']
-        @legacy_ocr_search_results = @legacy_ocr_search_request['response']['docs']
+        @legacy_ocr_search_results = []
+        for hit in @legacy_ocr_search_request['response']['docs'] do
+            stripped=hit['key'].gsub(@prefix+'.'+@documentId+'.', '')
+            @legacy_ocr_search_results.append(stripped)
+        end
     end
 end
