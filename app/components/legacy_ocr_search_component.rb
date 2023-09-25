9dbc9ad79d8f12bc8397e2b3df7ae45b4d2d444c
Brittny Lapierre <brittny.lapierre@gmail.com>
2023-09-25 16:15:56 -0400

    Sort search results

--- app/components/legacy_ocr_search_component.rb
+++ app/components/legacy_ocr_search_component.rb
@@ -4,14 +4,18 @@ class LegacyOcrSearchComponent < ViewComponent::Base
         @documentId = documentId
         @term = term
         @prefix = prefix
-        rsolr = RSolr.connect :url => 'http://tokaji.tor.c7a.ca:8983/solr/cosearch2'
-        @legacy_ocr_search_request= rsolr.get 'select', :params => {
-            :q => 'tx:' + term + ' AND ' + 'pkey:' + @prefix + '.' + @documentId
-        } 
-        @legacy_ocr_search_results = []
-        for hit in @legacy_ocr_search_request['response']['docs'] do
-            stripped=hit['key'].gsub(@prefix+'.'+@documentId+'.', '')
-            @legacy_ocr_search_results.append(stripped)
+        puts term.length
+        if @term.length != 0
+            rsolr = RSolr.connect :url => 'http://tokaji.tor.c7a.ca:8983/solr/cosearch2'
+            @legacy_ocr_search_request= rsolr.get 'select', :params => {
+                :q => 'tx:' + term + ' AND ' + 'pkey:' + @prefix + '.' + @documentId
+            } 
+            legacy_ocr_search_results_unsorted = []
+            for hit in @legacy_ocr_search_request['response']['docs'] do
+                stripped=hit['key'].gsub(@prefix+'.'+@documentId+'.', '')
+                legacy_ocr_search_results_unsorted.append(stripped)
+            end
+            @legacy_ocr_search_results = legacy_ocr_search_results_unsorted.sort
         end
     end
 end
